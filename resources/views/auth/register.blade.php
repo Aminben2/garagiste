4bb4b06e93b5d134e80cef0e183d61d7e47a6c4d
Aminben2 <aminbenomar454@gmail.com>
2024-03-13 16:22:36 +0000

    little modifications

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -92,7 +92,7 @@
             <div class="row g-0">
                 <div
                     class="col-12 col-xl-7 col-xxl-8 auth-cover-left align-items-center justify-content-center d-none d-xl-flex">
-                    <div class="card shadow-none bg-transparent shadow-none rounded-0 mb-0">
+                    <div class="card bg-transparent shadow-none rounded-0 mb-0">
                         <div class="card-body">
                             <img src="{{ asset('assets/images/login-images/register-cover.svg') }}"
                                 class="img-fluid auth-img-cover-login" width="550" alt="" />
